9ae2bc3d69c0609a0f61c15541a1dfb7afa35734
mshirdel <mshirdel@gmail.com>
2021-07-18 17:28:31 +0430

    add customized user model

---
+++ accounts/migrations/0001_postgres_extensions.py
@@ -0,0 +1,8 @@
+from django.contrib.postgres.operations import CITextExtension
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = []
+
+    operations = [CITextExtension()]
